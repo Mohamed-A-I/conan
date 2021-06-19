21281ba50dcab68fa5701273c4c1e858d0710553
hady993 <57845885+hady993@users.noreply.github.com>
2021-06-19 22:17:16 +0300

    Update person.py

--- code/person.py
+++ code/person.py
@@ -1,6 +1,8 @@
 class Person:
+    # A class that has the person's data
     def __init__(self):
-	
+
+	# The initialization function of the class
         self.name = ""
         self.tel = ""
         self.addr = ""
